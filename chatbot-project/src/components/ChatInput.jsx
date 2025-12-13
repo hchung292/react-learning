986cee6aee9d13572d2052ea53883f579d178996
Han Chung <hanchungtay9@gmail.com>
2025-12-13 15:05:53 +0800

    Lesson 5 Completed

---
+++ chatbot-project/src/components/ChatInput.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import { Chatbot } from 'supersimpledev'
+import LoadingSpinner from '../assets/loading-spinner.gif'
+
+export function ChatInput({ chatMessages, setChatMessages }) {
+    const [inputText, setInputText] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    function saveInputText(event) {
+        setInputText(event.target.value);
+    }
+
+    async function sendMessage() {
+
+      if (isLoading || inputText === '') {
+        return;
+      }
+
+    setIsLoading(true);
+
+    setInputText('');
+
+    const newChatMessages = [
+        ...chatMessages,
+        {
+            message: inputText,
+            sender: 'user',
+            id: crypto.randomUUID()
+        }
+    ];
+
+          setChatMessages([
+            ...newChatMessages,
+            {
+              message: <img className="loading-spinner" src={LoadingSpinner}/>,
+              sender: 'robot',
+              id: crypto.randomUUID()
+            }
+          ]);
+
+          const response = await Chatbot.getResponseAsync(inputText);
+
+          setChatMessages([
+            ...newChatMessages,
+            {
+              message: response,
+              sender: 'robot',
+              id: crypto.randomUUID()
+            }
+          ]);
+
+          setIsLoading(false);
+        }
+
+        function checkKey(event) {
+          if(event.key === 'Enter'){
+            sendMessage();
+          } else if (event.key === 'Escape') {
+            setInputText('');
+          }
+        }
+
+        return (
+          <div className="chat-input-container">
+            <input 
+              placeholder="Send a message to Chatbot" 
+              size="30"
+              className="chat-input" 
+              onChange={saveInputText}
+              value={inputText}
+              onKeyDown={checkKey}
+            ></input>
+            <button
+              onClick={sendMessage}
+              className="send-button"
+            >Send
+            </button>
+          </div>
+        );
+      }
---
+++ chatbot-project/src/components/ChatMessages.jsx
@@ -0,0 +1,45 @@
+import { useRef, useEffect } from 'react'
+import { ChatMessage } from './ChatMessage'
+
+// Custom Hook
+function useAutoScroll(dependencies) {
+  const containerRef = useRef(null);
+
+  useEffect(()=>{
+    const containerElem = containerRef.current;
+    if(containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  }, [dependencies]);
+
+  return containerRef;
+}
+
+function ChatMessages({ chatMessages }) {
+
+  const chatMessagesRef = useAutoScroll([chatMessages]);
+
+  //Array Destructuring
+  //const [chatMessages, setChatMessages] = array;
+
+  // const chatMessages = array[0];      //current value
+  // const setChatMessages = array[1];   // update function
+
+  return (
+    <div className="chat-messages-container" 
+      ref={chatMessagesRef}
+    >
+    {chatMessages.map((chatMessage) => {
+        return (
+          <ChatMessage 
+            message = {chatMessage.message}
+            sender = {chatMessage.sender}
+            key = {chatMessage.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default ChatMessages;
