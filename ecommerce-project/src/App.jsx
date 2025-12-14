8b51db4cb8aadd1b65265970a3d69872c6911f96
Han Chung <hanchungtay9@gmail.com>
2025-12-14 21:13:04 +0800

    Move orders and tracking page to react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,9 @@
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 import { Routes, Route } from 'react-router';
+
 import './App.css'
 
 function App() {
@@ -14,6 +17,16 @@ function App() {
       <Route 
         path="/checkout" 
         element={<CheckoutPage />}
+      />
+
+      <Route 
+        path="/orders" 
+        element={<OrdersPage />}
+      />
+
+        <Route 
+        path="/tracking" 
+        element={<TrackingPage />}
       />
       
     </Routes>
