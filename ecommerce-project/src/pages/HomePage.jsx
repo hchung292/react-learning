a13c1c02b640ab340d89a1672f2acb521191824b
Han Chung <hanchungtay9@gmail.com>
2025-12-15 18:40:37 +0800

    use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,14 +1,21 @@
+import axios from 'axios';
 import './HomePage.css';
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
 
 
 export function HomePage() {
-    fetch('http://localhost:3000/api/products')
+    // fetch('http://localhost:3000/api/products')
+    //     .then((response) => {
+    //         return response.json();
+    //     }).then((data) => {
+    //         console.log(data);
+    //     });
+
+    // Using axios
+        axios.get('http://localhost:3000/api/products')
         .then((response) => {
-            return response.json();
-        }).then((data) => {
-            console.log(data);
+            response.data
         });
 
     return (
