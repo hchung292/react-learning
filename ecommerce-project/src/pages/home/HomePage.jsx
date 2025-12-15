38b904d816fdfd768b147372fc68fd55c3f4bb25
Han Chung <hanchungtay9@gmail.com>
2025-12-15 23:52:43 +0800

    Convert home and checkout to async await

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './HomePage.css';
 import { Header } from '../../components/Header';
-import {ProductsGrid } from './ProductsGrid';
+
+import { ProductsGrid } from './ProductsGrid';
 
 export function HomePage({ cart }) {
     // fetch('http://localhost:3000/api/products')
@@ -14,13 +15,27 @@ export function HomePage({ cart }) {
 
     const [products, setProducts] = useState([]);
 
+    // Since in useEffect must return promise or clean code,
+    // hence need to create another function inside the useEffect
+    // This enable the async and await work
     useEffect(() => {
-        // Using axios
-        axios.get('/api/products')
-            .then((response) => {
-               setProducts(response.data);
-            });
+        const getHomeData = async () => {
+            // Using axios
+            const response = await axios.get('/api/products')
+            setProducts(response.data);
+        }
+
+        getHomeData();
     }, []);
+
+    // axios.then will return promise, in React is better not
+    // useEffect(() => {
+    //     // Using axios
+    //     axios.get('/api/products')
+    //         .then((response) => {
+    //            setProducts(response.data);
+    //         });
+    // }, []);
 
 
     return (
@@ -28,8 +43,8 @@ export function HomePage({ cart }) {
             <title>Ecommerce Project</title>
             <link rel="icon" type="image/svg+xml" href="/images/home-favicon.png" />
 
-            <Header 
-                cart={ cart }
+            <Header
+                cart={cart}
             />
 
             <div className="home-page">
